69ec76ad05c02b347482113475926d3fb75fb47f
nickhillwd <nickhillwd@gmail.com>
2016-01-26 11:04:50 +0000

    more tests added

--- waterBottle/waterBottle.js
+++ waterBottle/waterBottle.js
@@ -1,5 +1,23 @@
 var waterBottle = {
-  volume: 0
+  volume: 0,
+  fill: function(){
+    this.volume = 100;
+  },
+  sip: function(){
+    this.volume -= 10;
+  },
+  empty: function(){
+    this.volume = 0;
+  },
+  spill: function(){
+    this.volume -= 101;
+  },
+  notMinus: function(){
+    if(this.volume < 0){
+      console.log("Opps, think its all gone: bottle empty")
+      this.volume = 0;
+    }
+  }
 }
 
 //have to explicitly export the object to spec file, where it's required
--- waterBottle/waterBottleSpec.js
+++ waterBottle/waterBottleSpec.js
@@ -4,7 +4,30 @@ var assert = require('assert');
 
 //pass in string and function
 describe('Water Bottle', function(){
+  //shoule be empty at start
   it('should be empty at start', function(){
-    assert.equal(0, bottle.volume)
+    assert.equal(0, bottle.volume);
+  });
+  //should be 100 when full
+  it('should be 100 when full', function(){
+    bottle.fill();
+    assert.equal(100, bottle.volume);
+  });
+  //should go down by 10 when drink
+  it('should go down by 10 when drunk', function(){
+    bottle.sip();
+    assert.equal(90, bottle.volume);
+  });
+  //should go to 0 when emptied
+  it('should go to 0 when emptied', function(){
+    bottle.empty();
+    assert.equal(0, bottle.volume); 
+  });
+  //should not be able to go below 0
+  it('should not be able to go below 0', function(){
+    bottle.spill();
+    bottle.notMinus();
+    assert.equal(0, bottle.volume);
   });
 });
+
